e1a0d7026b159430e7075f27fe624e0541b9737a
Philipp Doerner <philippmdoerner@web.de>
2023-12-30 18:56:59 +0100

    Add example for illegal access

---
+++ examples/ex_stdinput_illegalaccess.nim
@@ -0,0 +1,71 @@
+import threadButler
+import std/[sugar, logging, options, strformat, os]
+
+addHandler(newConsoleLogger(fmtStr="[CLIENT $levelname] "))
+
+const CLIENT_THREAD = "client"
+const SERVER_THREAD = "server"
+type Response = distinct string
+type Request = ref object
+  text: ref string
+
+threadServer(CLIENT_THREAD):
+  messageTypes:
+    Response
+    
+  handlers:
+    proc handleResponseOnClient(msg: Response, hub: ChannelHub) =
+      debug "On Client: ", msg.string
+    
+threadServer(SERVER_THREAD):
+  properties:
+    startUp = @[
+      initEvent(() => addHandler(newConsoleLogger(fmtStr="[SERVER $levelname] "))),
+      initEvent(() => debug "Server startin up!")
+    ]
+    shutDown = @[initEvent(() => debug "Server shutting down!")]
+
+  messageTypes:
+    Request
+    
+  handlers:
+    proc handleRequestOnServer(msg: Request, hub: ChannelHub) = 
+      debug "On Server: ", cast[uint64](msg), " - ", getThreadId()
+
+      discard hub.sendMessage(Response("Handled: " & msg.text[]))
+
+prepareServers()
+
+proc runClientLoop(hub: ChannelHub) =
+  while IS_RUNNING:
+    echo "\nType in a message to send to the Backend!"
+    let terminalInput = readLine(stdin) # This is blocking, so this while-loop doesn't run and thus no responses are read unless the user puts something in
+    if terminalInput == "kill":
+      hub.sendKillMessage(ServerMessage)
+      break
+    
+    elif terminalInput.len() > 0:
+      let str = new(string)
+      str[] = terminalInput
+      let msg = Request(text: str)
+      debug "On Client: ", cast[uint64](msg), " - Thread: ", getThreadId()
+      discard hub.sendMessage(msg)
+      sleep(3000)
+      echo "Unsafe access: ", str[], msg[].repr
+    ## Guarantees that we'll have the response from server before we listen for user input again. 
+    ## This is solely for better logging, do not use in actual code.
+    sleep(100)
+    
+    let response: Option[ClientMessage] = hub.readMsg(ClientMessage)
+    if response.isSome():
+      routeMessage(response.get(), hub)
+
+proc main() =
+  let hub = new(ChannelHub)
+  
+  hub.withServer(SERVER_THREAD):
+    runClientLoop(hub)
+  
+  destroy(hub)
+
+main()
