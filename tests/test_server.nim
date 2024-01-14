b03723d7132162d804a22d4d3efc2ced2c94ca96
Philipp Doerner <philippmdoerner@web.de>
2024-01-14 14:29:25 +0100

    Add test about startup event and thread variable

--- tests/test_server.nim
+++ tests/test_server.nim
@@ -8,8 +8,16 @@ type Request = distinct int
 
 var responses: seq[int] = @[]
 var requests: seq[int] = @[]
+var clientThreadStartupCounter: int = 0
+var clientThreadShutdownCounter: int = 0
+var serverThreadStartupCounter: int = 0
+var serverThreadShutdownCounter: int = 0
 
 threadServer(CLIENT_THREAD):
+  properties:
+    startUp = @[initEvent(() => clientThreadStartupCounter.inc)]
+    shutDown = @[initEvent(() => clientThreadShutdownCounter.inc)]
+  
   messageTypes:
     Response
     
@@ -20,7 +28,9 @@ threadServer(CLIENT_THREAD):
 threadServer(SERVER_THREAD):
   properties:
     sleepMs = 50
-
+    startUp = @[initEvent(() => serverThreadStartupCounter.inc)]
+    shutDown = @[initEvent(() => serverThreadShutdownCounter.inc)]
+  
   messageTypes:
     Request
 
@@ -34,7 +44,8 @@ prepareServers()
 suite "Single Server Example":
   let hub = new(ChannelHub)
   
-  block simpleServer: 
+  block whenBlock: 
+    discard "When SERVER_THREAD gets started and the main thread sends 10 messages with the numbers 0-9"
     hub.withServer(SERVER_THREAD):
       for i in 0..<10:
         while not hub.sendMessage(i.Request): discard
@@ -43,8 +54,22 @@ suite "Single Server Example":
         var response: Option[ClientMessage] = hub.readMsg(ClientMessage)
         if response.isSome():
           routeMessage(response.get(), hub) 
-
+  
+  block thenBlock:
+    discard "Then SERVER_THREAD should have variable of thread 'serverButlerThread', ran once and be shut down"
+    check serverThreadStartupCounter == 1
+    check serverThreadShutdownCounter == 1
+    check serverButlerThread.running() == false
+  
+  block thenBlock:
+    discard "Then CLIENT_THREAD should have variable of thread 'clientButlerThread' which should have never run"
+    check clientThreadStartupCounter == 0
+    check clientThreadShutdownCounter == 0
+    check clientButlerThread.running() == false
+  
+  block thenBlock: 
+    discard "Then SERVER_THREAD should fill requests with the numbers 0-9 and send the responses 1-10 to the main thread"
     check requests == (0..9).toSeq(), "Server did not receive Requests correctly"
     check responses == (1..10).toSeq(), "Client did not receive Responses correctly"
-  
+
   hub.destroy()
